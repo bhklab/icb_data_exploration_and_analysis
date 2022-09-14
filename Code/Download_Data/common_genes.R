bf792a3b56db0767ad89d92ac7d09444e86f81f8
Minoru Nakano <minoru.nakano@gmail.com>
2022-09-14 12:32:07 +0200

    added common_genes txt file

--- Code/Download_Data/common_genes.R
+++ Code/Download_Data/common_genes.R
@@ -1,4 +1,5 @@
 library(stringr)
+library(data.table)
 
 load('Data/Gencode.v19.annotation.RData')
 features_gene_v19 <- features_gene
@@ -14,3 +15,5 @@ length(intersect(genes_v40, genes_v19)) # identical: 25545
 genes_v19_no_version <- str_replace_all(genes_v19, '\\.*', '')
 genes_v40_no_version <- str_replace_all(genes_v40, '\\.*', '')
 length(intersect(genes_v40_no_version, genes_v19_no_version)) #25545
+
+write.table(intersect(genes_v40_no_version, genes_v19_no_version), 'Data/common_genes.txt', col.names=FALSE, row.names = FALSE, quote=FALSE)
